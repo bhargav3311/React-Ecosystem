8ee0c6717341fce1ab0fe0b84f83e01971e0058b
unknown <nvn.bhargav@gmail.com>
2021-09-30 16:43:54 +0530

    - webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("pack");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node-modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@bebel/env"],
+        },
+
+        {
+            test:/\.css$/,
+            use:['style-loader', "css-loader"]
+        }
+      },
+    ],
+  },
+
+  resolve: {extensions: ['*', '.js', '.jsx']},
+  output:{
+      path: path.resolve(__dirname, 'dist/'),
+      publicPath: '/dist/',
+      filename: 'bundle.js'
+  },
+
+  devServer:{
+      contentBase: path.join(__dirname, 'public/'),
+      port: 3000,
+      publicPath: 'http://localhost:3000/dist/',
+      hotOnly: true
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
